6605a8b5c02f94b0f6ab466247dabb85ad6d21dd
sohag-aust <frustratedcoder96@gmail.com>
2021-09-07 21:46:30 +0600

    Reading data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListEmployeesComponent } from './employees/list-employees.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListEmployeesComponent
   ],
   imports: [
     BrowserModule,
