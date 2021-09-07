2c7dbc3fb9be4d852ccfe8d3dd2b67573855476e
sohag-aust <frustratedcoder96@gmail.com>
2021-09-07 23:42:10 +0600

    Routing and Navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// router module
+import {RouterModule, Routes} from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ListEmployeesComponent } from './employees/list-employees.component';
+import { CreateEmployeeComponent } from './employees/create-employee.component';
+
+// declaring routes
+const appRoutes: Routes = [
+  { path: 'list', component: ListEmployeesComponent},
+  { path: 'create', component: CreateEmployeeComponent},
+  { path: '', redirectTo: '/list', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListEmployeesComponent
+    ListEmployeesComponent,
+    CreateEmployeeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
